884619e3f3092037f7a3b659ff9d6726a76e5699
miguelalonsocoba <miguetheking53@gmail.com>
2024-08-16 11:53:05 -0600

    Initialized project

---
+++ src/main/java/com/cjava/demo/S06DevDemo01Application.java
@@ -0,0 +1,13 @@
+package com.cjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S06DevDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S06DevDemo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjava/demo/S06DevDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S06DevDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
